560b4e44df2ea3978993eaa5e8aaa5a83f1af8da
Cheng Li <cli130103@gmail.com>
2022-04-26 00:56:41 +1000

    Cutting unneeded things

--- app/src/main/java/com/example/guitarchordprogressionproject/MainActivity.kt
+++ app/src/main/java/com/example/guitarchordprogressionproject/MainActivity.kt
@@ -7,11 +7,11 @@ import android.widget.*
 import com.example.guitarchordprogressionproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-    lateinit var binding: ActivityMainBinding
-    var globalChordAmount = ""
-    var globalGenSelection = ""
-    var globalKeySelection = ""
-    var globalChordFlavor = ""
+    private lateinit var binding: ActivityMainBinding
+    private var globalChordAmount = ""
+    private var globalGenSelection = ""
+    private var globalKeySelection = ""
+    private var globalChordFlavor = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,7 +83,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private fun generateChordProgression() {
         // ChordAmount is assigned the value of the amount of chords the user wishes to generate.
         // This is from the spinner selection
-        var chordAmount = when(globalChordAmount) {
+        val chordAmount = when(globalChordAmount) {
             "3" -> 3
             "4" -> 4
             "5" -> 5
@@ -91,48 +91,48 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             else -> 7
         }
         var chordProgressionSequence = ""
-        var chordList = mutableListOf<Int>()
-        var random_no = 0
+        val chordList = mutableListOf<Int>()
+        var randomNo: Int
         // If the user wants a completely randomly generated chord progression
         if (globalGenSelection == "Random") {
             repeat(chordAmount) {
-                random_no = (1..7).random()
-                chordProgressionSequence = "$chordProgressionSequence $random_no"
-                chordList.add(random_no)
+                randomNo = (1..7).random()
+                chordProgressionSequence = "$chordProgressionSequence $randomNo"
+                chordList.add(randomNo)
             }
         }
         // If the user wants a chord progression based on the three common root movements,
         // The 3rd, 5th and step.
         else if (globalGenSelection == "3rd/5th/step root movements") {
-            var oneChord = ChordClass(6, 1)
+            val oneChord = ChordClass()
             oneChord.addChord(2)
             oneChord.addChord(3)
             oneChord.addChord(4)
             oneChord.addChord(5)
             oneChord.addChord(6)
             oneChord.addChord(7)
-            var twoChord = ChordClass(2, 2)
+            val twoChord = ChordClass()
             twoChord.addChord(5)
             twoChord.addChord(7)
-            var threeChord = ChordClass(2, 3)
+            val threeChord = ChordClass()
             threeChord.addChord(4)
             threeChord.addChord(6)
-            var fourChord = ChordClass(3, 4)
+            val fourChord = ChordClass()
             fourChord.addChord(2)
             fourChord.addChord(5)
             fourChord.addChord(7)
-            var fiveChord = ChordClass(3, 5)
+            val fiveChord = ChordClass()
             fiveChord.addChord(1)
             fiveChord.addChord(7)
-            var sixChord = ChordClass(2, 6)
+            val sixChord = ChordClass()
             sixChord.addChord(4)
             sixChord.addChord(2)
-            var sevenChord = ChordClass(3, 7)
+            val sevenChord = ChordClass()
             sevenChord.addChord(1)
             sevenChord.addChord(5)
             sevenChord.addChord(6)
 
-            val chordArray = mutableListOf<ChordClass>(oneChord, twoChord, threeChord, fourChord,
+            val chordArray = mutableListOf(oneChord, twoChord, threeChord, fourChord,
                 fiveChord, sixChord, sevenChord)
 
             var randomChosenChord = (1..7).random()
@@ -140,14 +140,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             chordList.add(randomChosenChord)
 
             repeat(chordAmount - 1) {
-                var tempArray = chordArray[randomChosenChord - 1].jumpingChords
+                val tempArray = chordArray[randomChosenChord - 1].jumpingChords
                 randomChosenChord = tempArray[(0 until tempArray.size).random()]
                 chordList.add(randomChosenChord)
                 chordProgressionSequence = "$chordProgressionSequence $randomChosenChord"
             }
         }
 
-        var chordProgressionString = printChordsInKey(chordList, globalKeySelection, globalChordFlavor)
+        val chordProgressionString = printChordsInKey(chordList, globalKeySelection, globalChordFlavor)
 
 
         val resultTextView: TextView = findViewById(R.id.textView)
@@ -160,12 +160,11 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
         // parent.getItemAtPosition(pos) retrieves the item at the item selected spinner
-        var result = parent.getItemAtPosition(pos).toString()
+        val result = parent.getItemAtPosition(pos).toString()
 
         // Gets the id for the spinner that has been selected
-        var activityId = parent.id
 
-        when (activityId) {
+        when (parent.id) {
             R.id.spinner -> globalChordAmount = result
             R.id.spinner2 -> globalGenSelection = result
             R.id.spinner3 -> globalKeySelection = result
@@ -184,36 +183,36 @@ fun printChordsInKey(chordList: MutableList<Int>, key: String, flavor: String): 
     var returnString = ""
 
     val keySet = when (key) {
-        "A" -> listOf<String>("A","B","C#","D","E","F#","G#")
-        "A#/b♭" -> listOf<String>("b♭","C","D","E♭","F","G", "A")
-        "B" -> listOf<String>("B", "C#", "D#", "E", "F#", "G#", "A#")
-        "C" -> listOf<String>("C","D","E","F","G","A","B")
-        "C#/D♭" -> listOf<String>("C#/D♭","B","C#","D","E","F#","G#")
-        "D" -> listOf<String>("D", "E", "F#", "G", "A", "B", "C#")
-        "D#/E♭" -> listOf<String>("E♭", "F", "G", "A♭", "B♭", "C", "D")
-        "E" -> listOf<String>("E", "F#", "G#", "A", "B", "C#", "D#", "E")
-        "F" -> listOf<String>("F", "G", "A", "B♭", "C", "D", "E", "F")
-        "F#/G♭" -> listOf<String>("F#", "G#", "A#", "B", "C#", "D#", "F")
-        "G" -> listOf<String>("G", "A", "B", "C", "D", "E", "F#")
-        else -> listOf<String>("A♭", "B♭", "C", "D♭", "E♭", "F", "G")
+        "A" -> listOf("A","B","C#","D","E","F#","G#")
+        "A#/b♭" -> listOf("b♭","C","D","E♭","F","G", "A")
+        "B" -> listOf("B", "C#", "D#", "E", "F#", "G#", "A#")
+        "C" -> listOf("C","D","E","F","G","A","B")
+        "C#/D♭" -> listOf("C#/D♭","B","C#","D","E","F#","G#")
+        "D" -> listOf("D", "E", "F#", "G", "A", "B", "C#")
+        "D#/E♭" -> listOf("E♭", "F", "G", "A♭", "B♭", "C", "D")
+        "E" -> listOf("E", "F#", "G#", "A", "B", "C#", "D#", "E")
+        "F" -> listOf("F", "G", "A", "B♭", "C", "D", "E", "F")
+        "F#/G♭" -> listOf("F#", "G#", "A#", "B", "C#", "D#", "F")
+        "G" -> listOf("G", "A", "B", "C", "D", "E", "F#")
+        else -> listOf("A♭", "B♭", "C", "D♭", "E♭", "F", "G")
     }
-    var key = ""
+    var keyIndex: String
     for (item in chordList) {
-        key = keySet[item - 1]
+        keyIndex = keySet[item - 1]
         returnString = if (flavor == "Normal") {
             when (item) {
-                1, 4, 5 -> "$returnString $key" + "Maj"
-                2, 3, 6 -> "$returnString $key" + "m"
-                else -> "$returnString $key" + "dim"
+                1, 4, 5 -> "$returnString $keyIndex" + "Maj"
+                2, 3, 6 -> "$returnString $keyIndex" + "m"
+                else -> "$returnString $keyIndex" + "dim"
             }
         }
 
         else {
             when (item) {
-                1, 4, 5 -> "$returnString $key" +"Maj7"
-                5 -> "$returnString $key" + "Dom7"
-                2, 3, 6 -> "$returnString $key" + "m7"
-                else -> "$returnString $key" + "dim7"
+                1, 4 -> "$returnString $keyIndex" +"Maj7"
+                5 -> "$returnString $keyIndex" + "Dom7"
+                2, 3, 6 -> "$returnString $keyIndex" + "m7"
+                else -> "$returnString $keyIndex" + "dim7"
             }
         }
     }
@@ -221,7 +220,7 @@ fun printChordsInKey(chordList: MutableList<Int>, key: String, flavor: String): 
 }
 
 
-class ChordClass(private val jChordsAmount: Int, private val chordId: Int) {
+class ChordClass {
     val jumpingChords: MutableList<Int> = mutableListOf()
 
     fun addChord(addedChord: Int) {
